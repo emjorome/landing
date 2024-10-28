1dc5662fd4dd84febc9b0b869cd9c3ab5847d0ff
emjorome <emjorome@espol.edu.ec>
2024-10-28 10:49:37 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+const databaseURL = 'https://landing-86a93-default-rtdb.firebaseio.com/coleccion.json'; 
+let sendData = () => {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+
+    // Realiza la petición POST con fetch
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+        form.reset()
+    })
+    .catch(error => {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    });
+}
+
 let ready = () => {
     console.log('DOM está listo')
 }
@@ -14,7 +43,10 @@ let loaded = ( eventLoaded ) => {
 
         if (emailText.length === 0) {
           emailElement.focus()
+
+          return;
         }
+        sendData();
     })
 
     emailElement.animate(
@@ -30,7 +62,9 @@ let loaded = ( eventLoaded ) => {
         }
     )
 
-  }
+}
+
+
 
 window.addEventListener("DOMContentLoaded", ready);
 window.addEventListener("load", loaded)
